bc176481f796c24183823eba59329f3b39d2df3f
Paulo Moreira <phmoreira37@gmail.com>
2023-05-20 16:34:27 -0300

    feat: change page element with components

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,24 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito por{' '}
+      <a
+        className="underline hover:text-gray-100"
+        href="https://www.linkedin.com/in/pauloh-moreira/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Paulo Moreira
+      </a>{' '}
+      | Acesse o código do projeto no{' '}
+      <a
+        className="underline hover:text-gray-100"
+        href="https://github.com/paulohmoreira/spacetime-web"
+        target="_blank"
+        rel="noreferrer"
+      >
+        GitHub
+      </a>
+    </div>
+  )
+}
