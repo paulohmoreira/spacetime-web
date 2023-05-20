6a9d0b14212d225e63467ff91dee31dce58cd473
Paulo Moreira <phmoreira37@gmail.com>
2023-05-20 09:43:21 -0300

    feat: add Stripes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         sans: 'var(--font-roboto)',
         alt: 'var(--font-bai-jamjuree)',
       },
+
       colors: {
         gray: {
           50: '#eaeaea',
@@ -49,6 +50,16 @@ module.exports = {
           900: '#025929',
         },
       },
+
+      backgroundImage: {
+        stripes:
+          'linear-gradient(to bottom, rgba(255,255,255,0.1), rgba(255,255,255,0.1) 12.5%, transparent 12.5%, transparent)',
+      },
+
+      backgroundSize: {
+        stripes: '100% 8px',
+      },
+
       blur: {
         full: '194px',
       },
